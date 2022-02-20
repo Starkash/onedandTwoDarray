61a116995bef2746baa078bc24018cf2777f5b74
Starkash <99684327+Starkash@users.noreply.github.com>
2022-02-20 19:50:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace OneD
+{
+    class Array1d
+    {
+        static void Main()
+        {
+            int[,,] Arr = new int[3, 1, 4] { { { 0, 4, 8, 10 } }, { { 5, 6, 45, 5 } }, { { 5, 58, 95, 5 } } };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Console.Write(Arr[i, j, k] + " ");
+                        Console.WriteLine();
+                    }
+                }
+                
+            }
+            Console.ReadLine();
+
+                    
+
+
+                
+        }
+    }
+}
+
+
+
+
+
+
+
+
